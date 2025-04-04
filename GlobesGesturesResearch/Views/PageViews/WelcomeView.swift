554990dec049a1b5aa48c79417404241abf60208
Bernie Jenny <berniejenny@users.noreply.github.com>
2025-04-04 15:36:58 +1100

    Simplify WelcomeView

--- GlobesGesturesResearch/Views/PageViews/WelcomeView.swift
+++ GlobesGesturesResearch/Views/PageViews/WelcomeView.swift
@@ -8,130 +8,64 @@
 import SwiftUI
 
 /// The first view shown.
-//struct WelcomeView: View {
-//    @Binding var currentPage: Page
-//    
-//    var body: some View {
-//        VStack {
-//            Text("Hi There! Welcome to Gestures for Globes User Study")
-//                .font(.largeTitle)
-//            Text("Hi")
-//                .font(.body)
-//            Button("Let's begin") {
-//                currentPage = currentPage.next()
-//            }
-//        }
-//    }
-//}
-
 struct WelcomeView: View {
     @Binding var currentPage: Page
     @Environment(ViewModel.self) var model
     
-    @State private var isButtonPressed: Bool = false
-    
     var body: some View {
         ZStack {
-            VStack {
+            HStack {
                 Spacer()
-                VStack(alignment: .trailing){
+                VStack(alignment: .trailing) {
                     Spacer()
-                    HStack{
-                        Spacer()
-                    }
                     if TaskStorageManager.storageFileRead {
-                        HStack {
-                            Text("Storage file is properly loaded and ready")
-                                .foregroundColor(.cyan)
-                            Image(systemName: "checkmark.circle.fill")
-                                .foregroundColor(.green)
-                        }
+                        Label("Storage file is loaded and ready.", systemImage: "checkmark.circle.fill")
+                            .foregroundColor(.green)
+                        
                     } else {
-                        HStack {
-                            Text("Storage file is not properly loaded")
-                                .foregroundColor(.yellow)
-                            Image(systemName: "exclamationmark.circle.fill")
-                                .foregroundColor(.yellow)
-                        }
+                        Label("Storage file is not loaded.", systemImage: "exclamationmark.circle.fill")
+                            .foregroundColor(.yellow)
                     }
-                }
-                
-                HStack{
-                    Spacer()
+                    
                     if !model.positionConditions.isEmpty && !model.rotationConditions.isEmpty && !model.scaleConditions.isEmpty {
-                        HStack {
-                            Text("Study conditions are properly loaded and ready")
-                                .foregroundColor(.cyan)
-                            Image(systemName: "checkmark.circle.fill")
-                                .foregroundColor(.green)
-                        }
+                        Label("Study conditions are loaded and ready.", systemImage: "checkmark.circle.fill")
+                            .foregroundColor(.green)
                     } else {
-                        HStack {
-                            Text("Study conditions are not properly loaded")
-                                .foregroundColor(.yellow)
-                            Image(systemName: "exclamationmark.circle.fill")
-                                .foregroundColor(.yellow)
-                        }
+                        Label("Study conditions are not loaded.", systemImage: "exclamationmark.circle.fill")
+                            .foregroundColor(.yellow)
                     }
                 }
             }
-            .padding(40)
-            .background(RoundedRectangle(cornerRadius: 15).fill(Color(.systemGray4)).shadow(radius: 5))
-            .padding(.horizontal, 40)
-            VStack {
-                Spacer()
-                VStack{
-                    Text("Welcome to the User Study")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .multilineTextAlignment(.center)
-                    
-                    
-                    Text("To move the window, look at the bar in the bottom, pinch and move around.")
-                    .font(.body)
+            .padding(50)
+            
+            VStack(spacing: 50) {
+                Text("Welcome to the User Study")
+                    .font(.largeTitle)
                     .multilineTextAlignment(.center)
-                    .padding()
-                    
-                    Button(action: {
-                        TaskStorageManager.shared.initialiseUserID()
-                        withAnimation(.easeInOut(duration: 0.3)) {
-                                            currentPage = currentPage.next() //
-                                            isButtonPressed.toggle()
-                                        }
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-                            withAnimation(.easeInOut(duration: 0.2)) {
-                                isButtonPressed = false
-                            }
-                        }
-                    }){
-                        Text("Let's begin")
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            .cornerRadius(15)
-                            .shadow(color: .black, radius: 1)
-
+                
+                Text("To move the window, look at the bar below the window, then pinch and move.")
+                    .multilineTextAlignment(.center)
+                
+                Button("Start", action: {
+                    TaskStorageManager.shared.initialiseUserID()
+                    withAnimation(.easeInOut(duration: 0.3)) {
+                        currentPage = currentPage.next()
                     }
-                    .bold()
-                    .background(Color.blue)
-                    .cornerRadius(20)
-                    .scaleEffect(isButtonPressed ? 1.1 : 1.0)
-                    .shadow(color: .black.opacity(0.3), radius: 10, x: 0, y: 5)
-                }
-                .padding()
-                .background(RoundedRectangle(cornerRadius: 15).fill(Color(.systemGray2)).shadow(radius: 5))
-                .padding(.horizontal, 40)
-                Spacer()
+                })
+                .tint(.accentColor)
             }
+            .padding(100)
         }
+        
         .onAppear{
             SoundManager.shared.playSound(named: "enterAndExit")
         }
     }
 }
-    
 
-//#Preview {
-//    WelcomeView(currentPage: .constant(.welcome))
-//        .environment(ViewModel())
-//        .environment(StudyModel())
-//}
+#Preview {
+    WelcomeView(currentPage: .constant(.welcome))
+        .glassBackgroundEffect()
+        .environment(ViewModel())
+        .environment(StudyModel())
+}
