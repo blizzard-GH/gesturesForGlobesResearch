4f3c1d483e23d7c30d913ea72ede58e6197d77e1
Bernie Jenny <berniejenny@users.noreply.github.com>
2025-04-14 14:58:33 +1000

    Fix RotationMatcher

--- GlobesGesturesResearch/Model/Matcher/RotationMatcher.swift
+++ GlobesGesturesResearch/Model/Matcher/RotationMatcher.swift
@@ -11,44 +11,21 @@ import RealityKit
 @Observable
 class RotationMatcher: Matcher {
     let rotationTarget: simd_quatf
-    let tolerance: Float = 0.6
-    
-    private let soundManager: SoundManager
-
-    
-    init(rotationTarget: simd_quatf, soundManager: SoundManager) {
+    let tolerance: Float = 10 * .pi * 180
+        
+    init(rotationTarget: simd_quatf) {
         self.rotationTarget = rotationTarget
-        self.soundManager = soundManager
     }
     
     func isMatching(_ transform: Transform) -> Bool {
         let angleDifference = quaternionAngleDifference(q1: transform.rotation, q2: rotationTarget)
-        let matched = angleDifference <= tolerance
-
-        return matched
+        return angleDifference <= tolerance
     }
     
     func quaternionAngleDifference(q1: simd_quatf, q2: simd_quatf) -> Float {
-        // Calculate the dot product of the two quaternions and make it absolute
-//        var dotProduct = simd_dot(q1.vector, q2.vector)
-//        
-//        if dotProduct < 0 {
-//            dotProduct = -dotProduct
-//        }
-//        
-//        // Avoiding numerical errors
-//        let clampedDot = max(-1.0, min(1.0, dotProduct))
-        // Normalised
-        let normQ1 = simd_normalize(q1)
-        let normQ2 = simd_normalize(q2)
-        
-        // Compute the dot product and clamp to avoid numerical issues
-        let dotProduct = simd_dot(normQ1.vector, normQ2.vector)
-        let clampedDot = max(-1.0, min(1.0, abs(dotProduct)))
-        
-        // Measure the angle between the two quaternions
-        let angle = 2.0 * acos(clampedDot)
-        
+        let v1 = q1.act([0, 1, 0])
+        let v2 = q2.act([0, 1, 0])
+        let angle = acos(simd_dot(v1, v2))
         return angle
     }
     
